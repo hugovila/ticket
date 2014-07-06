6e423bf4a24aed6ec52ff0e9506e8f710b919013
Hugo Vila <git@hugovila.nom.es>
2014-07-06 03:25:32 +0200

    Added more internationalization terms

--- ticket_dispenser.rb
+++ ticket_dispenser.rb
@@ -2,15 +2,18 @@ require 'rubygems'
 require 'sinatra'
 require 'i18n'
 
+set :bind, '0.0.0.0'
+
 configure do
   I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
   I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
   I18n.backend.load_translations
 end
 
-PROJECT_NAME = "Ticket Dispenser"
 COMPANY      = "Devscola"
 AUTHOR       = "Hugo Vila"
+
+time = Time.new
 
 I18n.load_path = Dir['./locales/*.yml', './locales/*.rb']
 I18n.locale = :en
@@ -24,7 +27,7 @@ end
 
 
 before '/' do
-redirect to("/#{obtain_browser_locale}/")
+  redirect to("/#{obtain_browser_locale}/")
 end
 
 before '/:locale/*' do
@@ -43,10 +46,9 @@ get '/' do
   data[:url_prefix_locale] = "/#{I18n.locale}"
 
 
-  data[:obtain_languages_locales_site_chain] = obtain_languages_locales_site_chain
 
-	data[:company] = COMPANY
-	data[:author] = AUTHOR
+  data[:obtain_languages_locales_site_chain] = obtain_languages_locales_site_chain
+  data[:year] = time.year
 
 	erb :get_your_ticket, :layout => :layout_home, locals: { data: data }
 end
@@ -63,27 +65,18 @@ get '/ticket/data' do
   data[:url_prefix_locale] = "/#{I18n.locale}"
 
 
-  data[:obtain_language_locale_site_en] = obtain_language_locale_site(:en)
-  data[:obtain_language_locale_site_es] = obtain_language_locale_site(:es)
-  data[:obtain_language_locale_site_fr] = obtain_language_locale_site(:fr)
-
-  data[:path_info] = request.path_info
 
   data[:obtain_languages_locales_site_chain] = obtain_languages_locales_site_chain
-	
-	data[:company] = COMPANY
-	data[:author] = AUTHOR
+  data[:year] = time.year
 
 	erb :ticket_data, :layout => :layout_data, locals: { data: data }
 end
 
-before 'ticket/template' do
+
+
+before '/ticket/template' do
   # params[:when] = convert_data_form(params[:when])
 end
-
-# get '/ticket/template' do
-#   redirect_to_post
-# end
 
 
 post '/ticket/template' do
@@ -96,20 +89,13 @@ post '/ticket/template' do
   data[:url_prefix_locale] = "/#{I18n.locale}"
 
 
-  data[:I18n_available_locales] = I18n.available_locales  # Borrar
 
- params[:when] = convert_data_form(params[:when])
+params[:when] = convert_data_form(params[:when])
 
-  data[:obtain_languages_locales_site_0] = obtain_languages_locales_site[0]
-  data[:obtain_languages_locales_site_1] = obtain_languages_locales_site[1]
-  data[:obtain_languages_locales_site_2] = obtain_languages_locales_site[2]
-
-  data[:path_info] = request.path_info
 
   data[:obtain_languages_locales_site_chain] = obtain_languages_locales_site_chain
-	
-	data[:company] = COMPANY
-  data[:author] = AUTHOR
+	data[:path_info] = request.path_info
+  data[:year] = time.year
 
 	erb :ticket_templates, :layout => :layout_templates, locals: { data: data }
 end
@@ -126,10 +112,9 @@ post '/ticket/print' do
   data[:url_prefix_locale] = "/#{I18n.locale}"
 
 
-  data[:I18n_available_locales] = I18n.available_locales  # Borrar
-
-  data[:company] = COMPANY
-  data[:author] = AUTHOR
+  data[:obtain_languages_locales_site_chain] = obtain_languages_locales_site_chain
+  data[:path_info] = request.path_info
+  data[:year] = time.year
 	
 	renderize(params[:template], locals: { data: data })
 
@@ -146,11 +131,12 @@ get '/ticket/print/:name/:surname/:where/:when/:template' do
 
   data[:url_prefix_locale] = "/#{I18n.locale}"
 
-  data[:I18n_available_locales] = I18n.available_locales  # Borrar
   
-
-  data[:company] = COMPANY
-  data[:author] = AUTHOR
+  data[:obtain_languages_locales_site_chain] = obtain_languages_locales_site_chain
+  data[:path_info] = request.path_info
+  #data[:company] = COMPANY
+  #data[:author] = AUTHOR
+  data[:year] = time.year
 	
 	renderize(params[:template], locals: { data: data })
 
@@ -166,11 +152,12 @@ get '/ticket/print/:name/:surname/:where/:when_day/:when_month/:when_year/:templ
 
   data[:url_prefix_locale] = "/#{I18n.locale}"
 
-  data[:I18n_available_locales] = I18n.available_locales  # Borrar
-  
 
+  data[:obtain_languages_locales_site_chain] = obtain_languages_locales_site_chain
+  data[:path_info] = request.path_info
   data[:company] = COMPANY
   data[:author] = AUTHOR
+  data[:year] = time.year
   
   renderize(params[:template], locals: { data: data })
 
@@ -183,12 +170,11 @@ not_found do
   status 404
   data[:your_are_in] = "not found"
 
-  data[:company] = COMPANY
-  data[:author] = AUTHOR
+  data[:obtain_languages_locales_site_chain] = obtain_languages_locales_site_chain
+  data[:year] = time.year
 
   erb :not_found_404, :layout => :layout_not_found_404, locals: { data: data }
 end
-
 
 
 
